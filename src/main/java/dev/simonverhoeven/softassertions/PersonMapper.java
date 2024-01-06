a8c8afa1f23b39e7b8ac5c201c311afe002c8c73
Simon Verhoeven <verhoeven.simon@gmail.com>
2024-01-06 20:44:02 +0100

    feat: add a hardassert sample

--- src/main/java/dev/simonverhoeven/softassertions/PersonMapper.java
+++ src/main/java/dev/simonverhoeven/softassertions/PersonMapper.java
@@ -1,28 +1,8 @@
 package dev.simonverhoeven.softassertions;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 
 public class PersonMapper {
-
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-
-    public static Person recordToEntity(PersonDTO person) {
-        try {
-            return new Person(
-                    person.name(),
-                    person.mainLanguage(),
-                    person.email(),
-                    person.address(),
-                    person.phoneNumber(),
-                    LocalDate.parse(person.dateOfBirth())
-            );
-        } catch (DateTimeParseException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 
     public static PersonDTO entityToRecord(Person person) {
         return new PersonDTO(
@@ -31,7 +11,7 @@ public class PersonMapper {
                 person.getEmail(),
                 person.getAddress(),
                 person.getPhoneNumber(),
-                DATE_FORMAT.format(person.getDateOfBirth())
+                person.getDateOfBirth().toString()
         );
     }
 }
--- src/test/java/dev/simonverhoeven/softassertions/JUnitHardTest.java
+++ src/test/java/dev/simonverhoeven/softassertions/JUnitHardTest.java
@@ -2,10 +2,50 @@ package dev.simonverhoeven.softassertions;
 
 import org.junit.jupiter.api.Test;
 
-class JUnitHardTest {
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JUnitHardTest {
+    
     @Test
     void hardAssert() {
+        Person person1 = new Person(
+                "John Doe",
+                "English",
+                "john.doe@example.com",
+                "123 Main St",
+                "555-1234",
+                LocalDate.of(1980, Month.DECEMBER, 1)
+        );
 
+        Person person2 = new Person(
+                "Jane Doe",
+                "French",
+                "jane.doe@example.com",
+                "456 Oak St",
+                "555-5678",
+                LocalDate.of(1982, Month.APRIL, 1)
+        );
+
+        var personRecord1 = PersonMapper.entityToRecord(person1);
+        var personRecord2 = PersonMapper.entityToRecord(person2);
+
+        assertEquals(person1.getName(), personRecord1.name());
+        assertEquals(person1.getMainLanguage(), personRecord1.mainLanguage());
+        assertEquals(person1.getEmail(), personRecord1.email());
+        assertEquals(person1.getAddress(), personRecord1.address());
+        assertEquals(person1.getPhoneNumber(), personRecord1.phoneNumber());
+        assertEquals(person1.getDateOfBirth().toString(), personRecord1.dateOfBirth());
+
+        // Assertions for person2
+        assertEquals(person2.getName(), personRecord2.name());
+        assertEquals(person2.getMainLanguage(), personRecord1.mainLanguage());
+        assertEquals(person2.getEmail(), personRecord2.email());
+        assertEquals(person2.getAddress(), personRecord2.address());
+        assertEquals(person2.getPhoneNumber(), personRecord1.phoneNumber());
+        assertEquals(person2.getDateOfBirth().toString(), personRecord1.dateOfBirth());
     }
 }
